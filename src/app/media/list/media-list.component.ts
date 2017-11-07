f94456057db25f031be7119e3abc5537b72686e1
Elena <olpetrushova@gmail>
2017-11-07 18:36:47 +0200

    height alignment of media cards

--- src/app/media/list/media-list.component.ts
+++ src/app/media/list/media-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {Media} from './../media';
 import {MediaService} from '../media.service';
